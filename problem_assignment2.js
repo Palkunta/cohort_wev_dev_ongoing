b5a3b47518210a93da112a6d82c4a1440f9ea678
Kuntal Pal <palkuntal9874@gmail.com>
2025-12-30 02:29:50 +0530

    some problem assignment of JS

---
+++ problem_assignment2.js
@@ -0,0 +1,25 @@
+// Problem: Create an object representing a type of tea with properties for
+// name, type, and caffeine content.
+const teas = {
+    name : "Lemon Tea" , 
+    "type of" : "Green-Tea" , 
+    caffine : "Low amount"
+}
+
+// Problem: Access and print the name and type properties of the tea object.
+console.log(teas.name);
+console.log(teas["type of"]);
+
+
+// Problem: Add a new property origin to the tea object.
+
+// Problem: Change the caffeine level of the tea object to "Medium".
+
+// Problem: Remove the type property from the tea object.
+
+// Problem: Check if the tea object has a property origin.
+
+// Problem: Use a for...in loop to print all properties of the tea object.
+
+// Problem: Create a nested object representing different types of teas and their
+// properties.
